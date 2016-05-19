ed34f52be77f4f5ff2a8438a70d1dc9026957b42
flight846 <flight846@gmail.com>
2016-05-19 15:58:33 +0000

    Passing the first sucessful test

---
+++ spec/features/user_creates_post_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Creating post" do
+    scenario 'succesfully' do
+        visit root_path
+        click_on "Write a story"
+        
+        fill_in "Title", with: "My first post"
+        fill_in "Body", with: "Some awesome content"
+        click_on "Publish"
+        
+        expect(page).to have_content "My first post"
+    end
+        
+    scenario 'unsuccessfully'
+end
