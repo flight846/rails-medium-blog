96edde4b8c8414adbe6a5c2f1ac8088168d6dea3
flight846 <flight846@gmail.com>
2016-05-20 16:09:58 +0000

    Allow users to create posts

--- spec/features/user_creates_post_spec.rb
+++ spec/features/user_creates_post_spec.rb
@@ -12,5 +12,14 @@ RSpec.describe "Creating post" do
         expect(page).to have_content "My first post"
     end
         
-    scenario 'unsuccessfully'
+    scenario 'unsuccessfully' do 
+        visit root_path
+        click_on "Write a story"
+        
+        fill_in "Title", with: "My second post"
+        fill_in "Body", with: ""
+        click_on "Publish"
+        
+        expect(page).to have_css ".error"
+    end
 end
