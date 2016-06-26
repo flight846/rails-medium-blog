43a48ec9871494de6667cf9f043471d392073b78
flight846 <flight846@gmail.com>
2016-06-26 07:00:33 +0000

    Add user edit and update methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#show'
   resources :posts, only: [:new, :create]
+  resources :users, only: [:show, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
